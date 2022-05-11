b9746d0688341d23c11ed7e89a63982929a1a26e
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2022-05-11 08:45:28 -0500

    Improve oauth module tests

--- test/ueberauth/strategy/amco/oauth_test.exs
+++ test/ueberauth/strategy/amco/oauth_test.exs
@@ -6,16 +6,14 @@ defmodule Ueberauth.Strategy.Amco.OAuthTest do
   alias Ueberauth.Strategy.Amco.Exceptions.MissingSiteConfiguration
 
   describe "client/1" do
-    test "raises error when site config is missing" do
+    test "raises error when missing site configuration" do
       assert_raise MissingSiteConfiguration, fn ->
         OAuth.client([])
       end
     end
 
     test "does not raise error when site config is present" do
-      site = "https://myidp.example.com"
-      client = OAuth.client(site: site)
-      assert client.site == site
+      assert OAuth.client(site: "https://myidp.example.com")
     end
   end
 end
